b7405d26227241b72921de82c193ac5af7e0f76f
cnegaa <snezhana.efremova.98@bk.ru>
2021-03-18 00:19:38 +1100

    footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-$(function() {
-
-});
+document.querySelector('.summary__conditions__toggler').addEventListener('click', () => {
+	const summaryCollapse = document.getElementById('summaryConditionsList')
+	const bsCollapse = new bootstrap.Collapse(summaryCollapse, {toggle: false})
+	bsCollapse.toggle()
+})
